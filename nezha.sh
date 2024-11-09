e3cdee9fd6deccba7443d3a57a2b8a720ef6a8da
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-11-09 13:25:29 +0800

    更新 nezha.sh

---
+++ nezha.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USER=$(whoami)
+WORKDIR="/home/${USER}/.nezha-agent"
+FILE_PATH="/home/${USER}/.s5"
+CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
+CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
+PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
+CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
+REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
+
+echo "检查并添加 crontab 任务"
+
+
+if [ -e "${WORKDIR}/start.sh" ]; then
+    echo "添加 nezha 的 crontab 重启任务"
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
+    echo "立即重启 nezha"
+    pkill -u $(whoami) -f nezha-agent # 杀掉当前用户下的所有 nezha-agent 进程
+    nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &
+fi
