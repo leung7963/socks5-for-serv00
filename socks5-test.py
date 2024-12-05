8621cd44d552256a3374f31083c5b1a9e8be2832
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-05 19:07:56 +0800

    更新 socks5-test.py

--- socks5-test.py
+++ socks5-test.py
@@ -1,11 +1,8 @@
 import socket
 import os
 
-
-def test_connection(domain_port_str):
+def test_connection(domain, port):
     try:
-        domain, port_str = domain_port_str.split(':')
-        port = int(port_str)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(3)  # 设置超时时间，单位秒
         result = sock.connect_ex((domain, port))
@@ -18,9 +15,6 @@ def test_connection(domain_port_str):
         print(f"{domain}:{port} 域名解析出错")
     except socket.timeout:
         print(f"{domain}:{port} 连接超时")
-    except ValueError:
-        print(f"{domain_port_str} 端口格式转换出错，请检查格式")
-
 
 
 # 获取GitHub环境变量中存放的域名和端口信息字符串
