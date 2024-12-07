1035d4c734bbd62483d340ba6bdca5692c498006
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-08 00:02:06 +0800

    更新 nezha.sh

--- nezha.sh
+++ nezha.sh
@@ -14,44 +14,33 @@ echo "检查并 重启 任务"
 
 # 检查进程是否在运行
 
-echo "检查NEZHA-AGENT"
-pgrep -x "nezha-agent" > /dev/null
+echo "检查php"
+pgrep -x "php" > /dev/null
 
 
 #如果没有运行，则启动 nezha
 if [ $? -ne 0 ]; then
-    nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &
-    echo "运行成功NEZHA-AGENT"
+    nohup ./vless/start.sh >/dev/null 2>&1 &
+    echo "运行成功php"
 fi
 
-echo "检查s5"
-pgrep -x "s5" > /dev/null
+echo "检查http"
+pgrep -x "http" > /dev/null
 
 
 if [ $? -ne 0 ]; then
-    nohup /home/$(whoami)/.s5/s5 -c /home/$(whoami)/.s5/config.json >/dev/null 2>&1 &
-    echo "s5运行成功"
+    nohup ./vless/http -c ./vless/config.json >/dev/null 2>&1 &
+    echo "http运行成功"
 fi
 
 
 
 # 检查进程是否在运行
-echo "检查singbox"
-pgrep -x "cloudflared" > /dev/null
+echo "检查node"
+pgrep -x "node" > /dev/null
 
 
 if [ $? -ne 0 ]; then
-    cd ./serv00-play/singbox
-    ./start.sh > /dev/null 2>&1
-    echo "singbox运行成功"
-fi
-
-# 检查进程是否在运行
-echo "检查singbox"
-pgrep -x "serv00sb" > /dev/null
-
-if [ $? -ne 0 ]; then
-    cd ./serv00-play/singbox
-    ./start.sh > /dev/null 2>&1
-    echo "singbox运行成功"
+    nohup ./vless/tunnel.sh >/dev/null 2>&1 &
+    echo "node运行成功"
 fi
